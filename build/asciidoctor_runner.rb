e08265c3d41c69137e64253890e7779e1dcc5e8d
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2022-02-02 08:54:41 -0500

    post 220201 classes.

--- build/asciidoctor_runner.rb
+++ build/asciidoctor_runner.rb
@@ -1,5 +1,5 @@
 require 'asciidoctor'
-Asciidoctor.convert_file 'C:\Users\jmanthony1\Documents\GitHub\Notes\build\intermediate\engr-851-001-integrated-computational-materials-engineering.adoc', to_file: 'C:\Users\jmanthony1\Documents\GitHub\Notes\docs\class\engr-851-001-integrated-computational-materials-engineering.html', safe: :unsafe, backend: 'html5', mkdirs: true, basedir: 'C:\Users\jmanthony1\Documents\GitHub\Notes\build\intermediate', attributes: 'imagesdir@=''images'' stylesheet@=''asciidoxy-no-toc.css'''
+Asciidoctor.convert_file 'C:\Users\jmanthony1\Documents\GitHub\Notes\build\intermediate\engr-727-001-advanced-mechanics-of-materials.adoc', to_file: 'C:\Users\jmanthony1\Documents\GitHub\Notes\docs\class\engr-727-001-advanced-mechanics-of-materials.html', safe: :unsafe, backend: 'html5', mkdirs: true, basedir: 'C:\Users\jmanthony1\Documents\GitHub\Notes\build\intermediate', attributes: 'imagesdir@=''images'' stylesheet@=''asciidoxy-no-toc.css'''
 logger = Asciidoctor::LoggerManager.logger
 exit 1 if (logger.respond_to? :max_severity) &&
   logger.max_severity &&
