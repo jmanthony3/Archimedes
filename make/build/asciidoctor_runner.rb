ad5d885049ebffdcdcbf696535fd4b525f396ff8
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2022-02-10 14:43:38 -0500

    post 220210 classes.

---
+++ make/build/asciidoctor_runner.rb
@@ -0,0 +1,7 @@
+require 'asciidoctor'
+require 'asciidoctor-bibtex'
+Asciidoctor.convert_file 'C:\Users\jmanthony1\Documents\GitHub\Notes\make\build\intermediate\engr-851-001-integrated-computational-materials-engineering.adoc', to_file: 'C:\Users\jmanthony1\Documents\GitHub\Notes\docs\class\engr-851-001-integrated-computational-materials-engineering.html', safe: :unsafe, backend: 'html5', mkdirs: true, basedir: 'C:\Users\jmanthony1\Documents\GitHub\Notes\make\build\intermediate', attributes: 'imagesdir@=''images'' stylesheet@=''asciidoxy-no-toc.css'' multipage'
+logger = Asciidoctor::LoggerManager.logger
+exit 1 if (logger.respond_to? :max_severity) &&
+  logger.max_severity &&
+  logger.max_severity >= (::Logger::Severity.const_get 'FATAL')
